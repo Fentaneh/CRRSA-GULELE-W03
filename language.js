5924877ca929db10c9d47dd0d89c96addfbcefbd
Fentaneh <fentaneh@gmail.com>
2026-07-23 16:53:30 +0300

    Update language.js

--- language.js
+++ language.js
@@ -1,6 +1,7 @@
 // ======================================
 // CRRSA Gulele Woreda 03
 // Global 3 Language System
+// Default Language: English
 // Amharic / English / Afaan Oromoo
 // ======================================
 
@@ -17,7 +18,7 @@ function changeLanguage(){
 
 
 
-// Safe Text Change Function
+// Safe Text Function
 
 function setText(id,text){
 
@@ -33,23 +34,24 @@ function setText(id,text){
 
 
 
-// Translate All Pages
+// Translate Page
 
 function translatePage(){
 
 
-let lang = localStorage.getItem("language") || "am";
+let lang = localStorage.getItem("language") || "en";
 
 
-// Keep selected language
+// keep dropdown selected
 
 let selector=document.getElementById("language");
 
 if(selector){
 
-selector.value=lang;
+    selector.value = lang;
 
 }
+
 
 
 
@@ -58,57 +60,47 @@ selector.value=lang;
 
 if(lang=="am"){
 
-setText(
-"officeTitle",
-"AACRRSA ጉለሌ ወረዳ 03"
-);
+
+setText("officeTitle",
+"AACRRSA ጉለሌ ወረዳ 03");
 
 
-setText(
-"pageTitle",
-"የሲቪል ምዝገባ እና የነዋሪነት አገልግሎት"
-);
+setText("pageTitle",
+"የሲቪል ምዝገባ እና የነዋሪነት አገልግሎት");
 
 
 }
 
 
 
-if(lang=="en"){
+else if(lang=="en"){
 
 
-setText(
-"officeTitle",
-"AACRRSA Gulele Woreda 03"
-);
+setText("officeTitle",
+"AACRRSA Gulele Woreda 03");
 
 
-setText(
-"pageTitle",
-"Civil Registration and Residency Service"
-);
+setText("pageTitle",
+"Civil Registration and Residency Service");
 
 
 }
 
 
 
-if(lang=="om"){
+else if(lang=="om"){
 
 
-setText(
-"officeTitle",
-"AACRRSA Aanaa Gulele 03"
-);
+setText("officeTitle",
+"AACRRSA Aanaa Gulele 03");
 
 
-setText(
-"pageTitle",
-"Tajaajila Galmee Lammummaa fi Jiraattotaa"
-);
+setText("pageTitle",
+"Tajaajila Galmee Lammummaa fi Jiraattotaa");
 
 
 }
+
 
 
 
@@ -116,42 +108,32 @@ setText(
 // ================= VITAL EVENTS =================
 
 
+
 if(lang=="am"){
 
 
-setText("heading","የወሳኝ ኩነት ምዝገባ");
+setText("heading",
+"የወሳኝ ኩነት ምዝገባ");
 
 
-setText("birthBtn","የልደት ምዝገባ");
-
-setText("marriageBtn","የጋብቻ ምዝገባ");
-
-setText("divorceBtn","የፍቺ ምዝገባ");
-
-setText("deathBtn","የሞት ምዝገባ");
-
-setText("adoptionBtn","የጉዲፈቻ ምዝገባ");
+setText("birthBtn",
+"የልደት ምዝገባ");
 
 
-}
+setText("marriageBtn",
+"የጋብቻ ምዝገባ");
 
 
-
-if(lang=="en"){
-
-
-setText("heading","Vital Events Registration");
+setText("divorceBtn",
+"የፍቺ ምዝገባ");
 
 
-setText("birthBtn","Birth Registration");
+setText("deathBtn",
+"የሞት ምዝገባ");
 
-setText("marriageBtn","Marriage Registration");
 
-setText("divorceBtn","Divorce Registration");
-
-setText("deathBtn","Death Registration");
-
-setText("adoptionBtn","Adoption Registration");
+setText("adoptionBtn",
+"የጉዲፈቻ ምዝገባ");
 
 
 }
@@ -159,24 +141,67 @@ setText("adoptionBtn","Adoption Registration");
 
 
 
-if(lang=="om"){
+else if(lang=="en"){
 
 
-setText("heading","Galmee Taateewwan Murteessoo");
+setText("heading",
+"Vital Events Registration");
 
 
-setText("birthBtn","Galmee Dhalootaa");
+setText("birthBtn",
+"Birth Registration");
 
-setText("marriageBtn","Galmee Gaa'elaa");
 
-setText("divorceBtn","Galmee Hiikkaa");
+setText("marriageBtn",
+"Marriage Registration");
 
-setText("deathBtn","Galmee Du'aa");
 
-setText("adoptionBtn","Galmee Guddifachaa");
+setText("divorceBtn",
+"Divorce Registration");
+
+
+setText("deathBtn",
+"Death Registration");
+
+
+setText("adoptionBtn",
+"Adoption Registration");
 
 
 }
+
+
+
+
+else if(lang=="om"){
+
+
+setText("heading",
+"Galmee Taateewwan Murteessoo");
+
+
+setText("birthBtn",
+"Galmee Dhalootaa");
+
+
+setText("marriageBtn",
+"Galmee Gaa'elaa");
+
+
+setText("divorceBtn",
+"Galmee Hiikkaa");
+
+
+setText("deathBtn",
+"Galmee Du'aa");
+
+
+setText("adoptionBtn",
+"Galmee Guddifachaa");
+
+
+}
+
 
 
 
@@ -184,63 +209,115 @@ setText("adoptionBtn","Galmee Guddifachaa");
 // ================= HOME SERVICES =================
 
 
+
 if(lang=="am"){
 
 
-setText("residentBtn","የነዋሪ አገልግሎት");
-
-setText("printingBtn","የህትመት አገልግሎት");
-
-setText("remarkBtn","ማስታወሻ አገልግሎት");
-
-setText("onlineIdBtn","የመስመር ላይ መታወቂያ");
+setText("residentBtn",
+"የነዋሪ አገልግሎት");
 
 
-}
+setText("printingBtn",
+"የህትመት አገልግሎት");
 
 
+setText("remarkBtn",
+"ማስታወሻ አገልግሎት");
 
-if(lang=="en"){
 
-
-setText("residentBtn","Resident Service");
-
-setText("printingBtn","Printing Service");
-
-setText("remarkBtn","Remark Service");
-
-setText("onlineIdBtn","Online ID");
+setText("onlineIdBtn",
+"የመስመር ላይ መታወቂያ");
 
 
 }
 
 
 
-if(lang=="om"){
+
+else if(lang=="en"){
 
 
-setText("residentBtn","Tajaajila Jiraataa");
+setText("residentBtn",
+"Resident Service");
 
-setText("printingBtn","Tajaajila Maxxansaa");
 
-setText("remarkBtn","Tajaajila Yaada Dabalataa");
+setText("printingBtn",
+"Printing Service");
 
-setText("onlineIdBtn","ID Karaa Interneetii");
+
+setText("remarkBtn",
+"Remark Service");
+
+
+setText("onlineIdBtn",
+"Online ID");
 
 
 }
 
 
+
+
+else if(lang=="om"){
+
+
+setText("residentBtn",
+"Tajaajila Jiraataa");
+
+
+setText("printingBtn",
+"Tajaajila Maxxansaa");
+
+
+setText("remarkBtn",
+"Tajaajila Yaada Dabalataa");
+
+
+setText("onlineIdBtn",
+"ID Karaa Interneetii");
+
+
 }
 
-// Run when page open
+
+
+}
+
+
+
+// ================= LOAD LANGUAGE =================
+
 
 window.onload=function(){
 
+
+let savedLanguage = localStorage.getItem("language");
+
+
+// First time = English
+
+if(!savedLanguage){
+
+    savedLanguage="en";
+
+    localStorage.setItem("language","en");
+
+}
+
+
+
+let selector=document.getElementById("language");
+
+
+if(selector){
+
+    selector.value=savedLanguage;
+
+}
+
+
+
 translatePage();
 
-};
-translatePage();
 
 };
-
