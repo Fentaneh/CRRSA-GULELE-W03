a03697b5758432b019d407a514fa6512a0cbdf50
Fentaneh <fentaneh@gmail.com>
2026-08-07 15:50:44 +0300

    Update config.php

--- config.php
+++ config.php
@@ -4,4 +4,19 @@ $botToken = "8534884253:AAFOcq70jnsEiGv-iUheOy0koOoVkp4G4HE";
 
 $chatID = "-1001574219465";
 
+/ =========================
+// Database Connection
+// =========================
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "crrsa_db";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Database Connection Failed: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8");
 ?>
