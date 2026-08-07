0969bfe2fbfedd6fa3783c3661ccf2ae7d8cd73a
Fentaneh <fentaneh@gmail.com>
2026-08-07 15:54:14 +0300

    Update save_appointment.php

--- save_appointment.php
+++ save_appointment.php
@@ -1,1 +1,39 @@
+<?php
+include "config.php";
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $appointment_no = $_POST["appointment_no"];
+    $full_name = $_POST["full_name"];
+    $phone = $_POST["phone"];
+    $service = $_POST["service"];
+    $application_date = $_POST["application_date"];
+    $appointment_date = $_POST["appointment_date"];
+
+    $sql = "INSERT INTO appointments
+    (appointment_no, full_name, phone, service, application_date, appointment_date)
+    VALUES (?, ?, ?, ?, ?, ?)";
+
+    $stmt = $conn->prepare($sql);
+
+    $stmt->bind_param(
+        "ssssss",
+        $appointment_no,
+        $full_name,
+        $phone,
+        $service,
+        $application_date,
+        $appointment_date
+    );
+
+    if ($stmt->execute()) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+
+    $stmt->close();
+}
+
+$conn->close();
+?>
